64c22a544a52088addd48269cd32094efbd3ceb1
Maxim Kloushin <makr1975@yandex.ru>
2017-01-17 12:49:46 +0100

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,8 @@ var socket = io();
 socket.on('connect', function(){
 	console.log('Connected to scoket.io server!');
 });
+
+socket.on('message', function(message){
+	console.log('New message');
+	console.log(message.text);
+});
