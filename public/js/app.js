a22bd8a55168367a5be35fc4e37f3d37d3491370
Maxim Kloushin <makr1975@yandex.ru>
2017-01-21 21:37:23 +0100

    Show timestamp on UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,28 @@
 var socket = io();
 
-socket.on('connect', function(){
-	console.log('Connected to scoket.io server!');
+socket.on('connect', function () {
+	console.log('Conncted to socket.io server!');
 });
 
-socket.on('message', function(message){
-	console.log('New message');
+socket.on('message', function (message) {
+	var momentTimestamp = moment.utc(message.timestamp);
+	console.log('New message:');
 	console.log(message.text);
 
-	jQuery('.messages').append('<p>' + message.text + '</p>')
+	jQuery('.messages').append('<p><strong>' + momentTimestamp.local().format('h:mm a') + ': </strong>' + message.text + '</p>');
 });
 
+// Handles submitting of new message
 var $form = jQuery('#message-form');
 
-$form.on('submit', function(event){
+$form.on('submit', function (event) {
 	event.preventDefault();
 
-var $message = $form.find('input[name=message]');
+	var $message = $form.find('input[name=message]');
 
 	socket.emit('message', {
 		text: $message.val()
 	});
 
 	$message.val('');
-	
 });
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var PORT =  process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
+var moment = require('moment');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -6,21 +7,24 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
 
-	socket.on('message', function(message){
-		console.log('Message received:' + message.text);
+	socket.on('message', function (message) {
+		console.log('Message received: ' + message.text);
 
+		message.timestamp = moment().valueOf();
 		io.emit('message', message);
-
 	});
 
-	socket.emit('message',{
-		text: 'Welcome to the chat application!'
+	// timestamp property - JavaScript timestamp (milliseconds)
+
+	socket.emit('message', {
+		text: 'Welcome to the chat application!',
+		timestamp: moment().valueOf()
 	});
 });
 
-http.listen(PORT, function(){
-	console.log('Server started');
+http.listen(PORT, function () {
+	console.log('Server started!');
 });
